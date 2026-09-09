bf9506818f683c5d5a4556988978fdce4ab0a765
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2026-09-09 18:33:33 +0600

    Initial implementation of the project structure and basic functionality.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div class="flex min-h-screen flex-col items-center justify-center gap-4 p-8">
+    <h1 class="text-3xl font-bold text-red-600">Hello World</h1>
+
+    <button
+      class="mt-4 rounded-lg bg-green-600 px-4 py-2 text-black hover:bg-green-700"
+    >
+      Click me
+    </button>
+  </div>
+</template>
